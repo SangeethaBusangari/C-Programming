d143007923c6f2c78f545c907ab9a99023c9cb5a
Sangeetha Busangari <sangeetha.785@gmail.com>
2014-05-30 12:37:08 +0530

    Update mergesort.c

--- recursion/mergesort.c
+++ recursion/mergesort.c
@@ -1,1 +1,56 @@
-
+#include <stdio.h>
+#define SIZE 25
+int num[SIZE];
+void mergesort(int low,int high)
+void merge(int low,int mid,int high)
+main()
+{
+    int i,n;
+    printf("enter number of elements\n");
+    scanf("%d",&n);
+    printf("enter %d elements");
+    for(i=1;i<=n;i++)
+      scanf("%d",&num[i]);
+    mergesort(1,n);
+    printf("After sorting\n");
+    for(i=1;i<=n;i++)
+    printf("%d\n",num[i]);
+}
+void mergesort(int low,int high)
+{
+  int mid;
+  if(low<high)
+  {
+    mid=(low+high)/2;
+    mergesort(low,mid);
+    mergesort(low+1,high);
+    merge(low,mid,high);
+  }
+}
+void merge(int low,int mid,int high)
+{
+  int temp[SIZE],la,ta,k,ha;
+  ta=low,la=low,ha=mid+1;
+  while(la<=mid&&ha<=high)
+  {
+    if(mun[la]<num[ha])
+      temp[ta]=num[la++];
+    else
+      temp[ta]=num[ha++];
+    ta++;
+  }
+  if(la<=mid)
+  {
+    for(k=la;k<=mid;k++)
+    {
+      temp[ta++]=num[k];
+    }
+  }
+  if(ha<=high)
+  {
+    for(k=ha;k<=high;k++,ta++)
+      temp[ta]=num[k];
+  }
+    for(k=low;k<=high;k++)
+      num[k]=temp[k];
+}
