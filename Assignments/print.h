b38a35a33a7115ec63dbf7603bddb6d668ab5082
Sangeetha Busangari <sangeetha.785@gmail.com>
2014-08-06 16:42:54 +0530

    Create print.h

---
+++ Assignments/print.h
@@ -0,0 +1,9 @@
+#include<stdio.h>
+print(input,result)
+{
+	if(input==result)
+		printf("The entered number %d is palindrome \n",input);
+	else
+		printf("the entered number %d is not a palindrome \n",input);
+
+}
